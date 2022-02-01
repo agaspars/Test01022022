f80bc6572352e74b2566833cb11708cbda92bdd5
Andrejs Gaspars <andrejs.gaspars@gmail.com>
2022-02-01 20:44:43 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gaspars1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Gaspars1
+            // 
+            this.Gaspars1.AutoSize = true;
+            this.Gaspars1.Location = new System.Drawing.Point(63, 64);
+            this.Gaspars1.Name = "Gaspars1";
+            this.Gaspars1.Size = new System.Drawing.Size(116, 17);
+            this.Gaspars1.TabIndex = 0;
+            this.Gaspars1.Text = "1 februaris 20:40";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 423);
+            this.Controls.Add(this.Gaspars1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Gaspars1;
+    }
+}
+
